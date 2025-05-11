bfc670645c908fb1f79cc99c7bf3497b52fbb616
Yaxing Cai <caiyaxing666@gmail.com>
2025-05-11 08:01:19 +0000

    fix

--- python/sglang/srt/layers/moe/fused_moe_flashinfer.py
+++ python/sglang/srt/layers/moe/fused_moe_flashinfer.py
@@ -8,8 +8,19 @@ from sgl_kernel import silu_and_mul
 
 import torch
 
-import flashinfer
-from flashinfer.gemm import group_gemm_fp8_nt_groupwise
+# import flashinfer
+# from flashinfer.gemm import group_gemm_fp8_nt_groupwise
+
+
+def fake_group_gemm(a, b, a_scale, b_scale, m_indptr, out):
+    cum_m, k = a.shape
+    e, n, _ = b.shape
+    assert b.shape[2] == k
+    assert a_scale.shape == (k // 128, cum_m)
+    assert b_scale.shape == (e, k // 128, n // 128)
+    assert out.shape == (cum_m, n)
+    assert m_indptr.shape[0] == e + 1
+    assert m_indptr[-1] == cum_m
 
 
 def fused_experts_flashinfer(
@@ -22,6 +33,7 @@ def fused_experts_flashinfer(
     topk_ids: torch.Tensor,
 ) -> torch.Tensor:
     from sglang.srt.layers.moe.fused_moe_triton.fused_moe import moe_align_block_size
+
     """
     This function computes a a8w8-quantized Mixture of Experts (MoE) layer
     using two sets of quantized weights, w1_q and w2_q, and top-k gating
@@ -42,7 +54,7 @@ def fused_experts_flashinfer(
     - out_dtype (torch.Tensor): The output tensor type.
     Returns:
     - torch.Tensor: The fp16 output tensor after applying the MoE layer.
-    """    
+    """
     print(
         f"Input shapes of fused_experts_flashinfer:\n"
         f"  a: {a.shape} (dtype: {a.dtype})\n"
@@ -60,10 +72,8 @@ def fused_experts_flashinfer(
     assert a.shape[1] == w1_q.shape[-1], "Hidden size mismatch"
     assert w1_q.shape[1] == w2_q.shape[2] * 2, "Hidden size mismatch"
     assert w1_q.shape[0] == w2_q.shape[0], "Weights expert number mismatch"
-    assert w1_q.shape[0] == w1_scale.shape[
-        0], "w1 scales expert number mismatch"
-    assert w1_q.shape[0] == w2_scale.shape[
-        0], "w2 scales expert number mismatch"
+    assert w1_q.shape[0] == w1_scale.shape[0], "w1 scales expert number mismatch"
+    assert w1_q.shape[0] == w2_scale.shape[0], "w2 scales expert number mismatch"
     assert a.dtype in [torch.half, torch.bfloat16], "Invalid output dtype"
 
     # a: torch.Size([256, 7168]),
@@ -80,51 +90,73 @@ def fused_experts_flashinfer(
     # w1_scale: torch.Size([257, 32, 56])
 
     out_dtype = a.dtype
-    num_experts = w1_q.size(0)
-    M = a.size(0) # 256
-    N = w1_q.size(1) # 4096
-    K = w2_q.size(1) # 7168
+    device = a.device
+    M = a.size(0)  # 256
+    N = w1_q.size(1)  # 4096
+    K = w2_q.size(1)  # 7168
+    E = w1_q.size(0)  # 257
 
-    E = w1_q.size(0) # 257
-
-    top_k = topk_ids.size(1) # 9
-
+    top_k = topk_ids.size(1)  # 9
 
     # preparation
     sorted_token_ids, expert_ids, num_tokens_post_padded = moe_align_block_size(
         topk_ids, 1, E
     )
-
-    # Group gemm 1
-    a_q, a1_scale = sglang_per_token_group_quant_fp8(a, 128)
-    # a_q: torch.Size([256, 7168]), dtype: torch.float8_e4m3fn, device: cuda:0
-    # a1_scale: torch.Size([256, 56]), dtype: torch.float32
-    device = a_q.device
-    A1 = torch.zeros((M * top_k, K), dtype=a_q.dtype, device=device)  # Keep as fp8
-    A1[::] = a_q[sorted_token_ids // top_k]  # Use quantized a_q
-    # Blocksize = 1
     output, count = expert_ids.unique_consecutive(return_counts=True)
-    C1 = torch.zeros((M * top_k, N), dtype=out_dtype, device=device)
     m_indptr = torch.zeros((E + 1,), dtype=torch.int32, device=device)
     m_indptr[output + 1] = count.int()
     m_indptr = m_indptr.cumsum(dim=0, dtype=m_indptr.dtype)
 
-    group_gemm_fp8_nt_groupwise(a=A1, b=w1_q, a_scale=a1_scale, b_scale=w1_scale, m_indptr=m_indptr, out=C1)
+    # Group gemm 1
+    # a_q: torch.Size([256, 7168]), dtype: torch.float8_e4m3fn, device: cuda:0
+    # a1_scale: torch.Size([256, 56]), dtype: torch.float32
+    device = a.device
+    a1 = torch.zeros((M * top_k, K), dtype=a.dtype, device=device)  # Keep as fp8
+    a1[::] = a[sorted_token_ids // top_k]  # Use quantized a_q
+    # Blocksize = 1
+    c1 = torch.zeros((M * top_k, N), dtype=out_dtype, device=device)
+    a1_q, a1_scale = sglang_per_token_group_quant_fp8(a1, 128, column_major_scales=True)
+    a1_scale = a1_scale.permute(1, 0)
+    w1_scale = w1_scale.permute(0, 2, 1).contiguous()
+    print(a1_q.shape, a1_scale.shape)
+    print(w1_q.shape, w1_scale.shape)
+    fake_group_gemm(
+        a=a1_q, b=w1_q, a_scale=a1_scale, b_scale=w1_scale, m_indptr=m_indptr, out=c1
+    )
+    # group_gemm_fp8_nt_groupwise(
+    #     a=a1_q, b=w1_q, a_scale=a1_scale, b_scale=w1_scale, m_indptr=m_indptr, out=C1
+    # )
+
     # C1: torch.Size([2304, 4096]), dtype: torch.bfloat16
 
     # silu and mul
-    intermediate = torch.empty((M * top_k, N * 2), device=device, dtype=out_dtype)
-    silu_and_mul(intermediate, C1)
+    intermediate = silu_and_mul(c1)
 
     # Group gemm 2
     intemediate_q, a2_scale = sglang_per_token_group_quant_fp8(
-        intermediate, 128)
+        intermediate, 128, column_major_scales=True
+    )
+    a2_scale = a2_scale.permute(1, 0)
+    w2_scale = w2_scale.permute(0, 2, 1).contiguous()
 
-    C2 = torch.zeros((M * top_k, K), dtype=out_dtype, device=device)
+    c2 = torch.zeros((M * top_k, K), dtype=out_dtype, device=device)
+    fake_group_gemm(
+        a=intemediate_q,
+        b=w2_q,
+        a_scale=a2_scale,
+        b_scale=w2_scale,
+        m_indptr=m_indptr,
+        out=c2,
+    )
+    # group_gemm_fp8_nt_groupwise(
+    #     a=intemediate_q,
+    #     b=w2_q,
+    #     a_scale=a2_scale,
+    #     b_scale=w2_scale,
+    #     m_indptr=m_indptr,
+    #     out=C2,
+    # )
 
-    group_gemm_fp8_nt_groupwise(a=intemediate_q, b=w2_q, a_scale=a2_scale, b_scale=w2_scale, m_indptr=m_indptr, out=C2)
-
-    # Create a new tensor in bfloat16 for the final result
-    res = torch.zeros((M * top_k, K), dtype=out_dtype, device=device)
-    res[sorted_token_ids] = C2[::]
-    return res.view((M, top_k, K)).sum(dim=1)
+    # reuse a1 for the final result
+    a1[sorted_token_ids] = c2[::]
+    return a1.view((M, top_k, K)).sum(dim=1)
